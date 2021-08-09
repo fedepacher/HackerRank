75a867372e8e703e9683fb01c386926ce2e6649f
fedepacher <fede_pacher@hotmail.com>
2021-08-09 16:50:42 -0300

    dia 13

---
+++ 30_days_of_code/day_13.py
@@ -0,0 +1,34 @@
+import time
+import math
+import os
+import random
+import re
+import sys
+from abc import ABCMeta, abstractmethod
+
+
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author  
+
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        Book.__init__(self, title, author)
+        self.price=price 
+
+    def display(self):
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Price: {self.price}")
+        
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
